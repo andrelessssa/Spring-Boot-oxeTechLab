6e4be30a2325ed1419182404043797d076d09e5e
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2025-07-15 19:53:49 -0300

    teste
    teste

--- cursospring/cursospring/src/main/java/br/com/java/oxetech/cursospring/CursospringApplication.java
+++ cursospring/cursospring/src/main/java/br/com/java/oxetech/cursospring/CursospringApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursospringApplication {
+public class CursospringApplication implements ComandLinerunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
+		System.out.println("Olá Alunos");
 	}
 
 }
