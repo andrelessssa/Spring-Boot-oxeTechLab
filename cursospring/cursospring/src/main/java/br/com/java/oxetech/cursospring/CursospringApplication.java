cfa1d245cf9fdcdaf44fff58f6c7070cea8e52ca
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2025-07-16 16:29:14 -0300

    err
    err

--- cursospring/cursospring/src/main/java/br/com/java/oxetech/cursospring/CursospringApplication.java
+++ cursospring/cursospring/src/main/java/br/com/java/oxetech/cursospring/CursospringApplication.java
@@ -1,7 +1,10 @@
 package br.com.java.oxetech.cursospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.java.oxetech.cursospring.service.MeusServico;
 
 @SpringBootApplication
 public class CursospringApplication{
@@ -9,6 +12,14 @@ public class CursospringApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
 		System.out.println("Olá Alunos");
+		
 	}
+	@Autowired
+	private MeusServico meusServico;
+
+	
+
+
+
 
 }
---
+++ cursospring/cursospring/src/main/java/br/com/java/oxetech/cursospring/service/MeusServico.java
@@ -0,0 +1,9 @@
+package br.com.java.oxetech.cursospring.service;
+
+public class MeusServico {
+
+    public void executar(){
+        System.out.println("Estou exetutando");
+    }
+    
+}
